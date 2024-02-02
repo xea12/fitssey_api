c1c151499e2445d63914d2e81332df19c34700e8
xea12 <37577367+xea12@users.noreply.github.com>
2024-02-02 12:26:43 +0100

    Update restapi.py

--- restapi.py
+++ restapi.py
@@ -3,8 +3,8 @@ import requests
 
 uuid = 'exshapebialystok'
 apiSource = 'test_michal'
-apiGuid = '31422255-D1F1-4E0C-8856-8E1A078EB0A5'
-apiKey = 'X5JHNRXXM57XG2N'
+apiGuid = ''
+apiKey = ''
 
 response = requests.get(f'https://app.fitssey.com/{uuid}/api/v4/public')
 
